68ac15c1285f8c3ca7c88a0eeee883db5a194ec5
Marcus costa <marcusprincipe@gmail.com>
2020-09-05 08:25:48 -0300

    selection sort

--- selection.c
+++ selection.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int main() {
-    int numbers[10] = {23, 2, 5, 8, 3, 43, 24, 78, 13, 9};
+int selection_s(int num[10]) {
     int start_pos = 0;
     int smallest = 0;
     int smallest_pos = 0;
@@ -10,25 +9,32 @@ int main() {
     while (start_pos < 10) {
         for (int i = start_pos ; i < 10; i++) {
             if ( i == start_pos ) {
-                smallest = numbers[start_pos];
+                smallest = num[start_pos];
                 smallest_pos = start_pos;
             }
             if ( i > start_pos ) {
-                if ( numbers[i] < smallest ) {
-                    smallest = numbers[i];
+                if ( num[i] < smallest ) {
+                    smallest = num[i];
                     smallest_pos = i;
                 }
             }
         }
-        if ( smallest < numbers[start_pos] ) {
-            tmp = numbers[start_pos];
-            numbers[start_pos] = smallest;
-            numbers[smallest_pos] = tmp;
+        if ( smallest < num[start_pos] ) {
+            tmp = num[start_pos];
+            num[start_pos] = smallest;
+            num[smallest_pos] = tmp;
             start_pos++;
         } else {
             start_pos++;
         }
     }
+   return 0; 
+}
+int main() {
+    int numbers[10] = {23, 2, 5, 8, 3, 43, 24, 78, 13, 9};
+    selection_s(numbers);    
+    //int sorted[10];
+    //sorted = selection_s(numbers);
     for (int j = 0; j < 10; j++) {
         printf("%i ", numbers[j]);
     }
